7ba31f4df879a54150fecc3abb52ad167bd2607f
Emil Duko <emil.duko@gmail.com>
2024-11-15 12:38:14 +0200

    Neveikia http requestas is WordListAPIService.

---
+++ Frontend/Services/WordListAPIService.cs
@@ -0,0 +1,35 @@
+using System.Text.Json;
+
+using PSInzinerija1.Exceptions;
+namespace Frontend.Services
+{
+    public class WordListAPIService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WordListAPIService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<string>?> GetWordsFromApiAsync(string fileName)
+        {
+            var response = await _httpClient.GetAsync($"api/wordlist/words?fileName={fileName}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new WordListLoadException("Failed to fetch words from the file.");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var wordList = JsonSerializer.Deserialize<List<string>>(content);
+
+            if (wordList == null)
+            {
+                throw new WordListLoadException("Failed to deserialize the word list from the file content.");
+            }
+
+            return wordList;
+        }
+    }
+}
