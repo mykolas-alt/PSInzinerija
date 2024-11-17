9aa89b88d46204a95679320c507d58b7528f8f1c
Augustas Kniška <kniskiukas@gmail.com>
2024-11-17 23:55:34 +0200

    Sukurtas controlleris GameStats

---
+++ Backend/Controllers/GameStatsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using PSInzinerija1.Services;
+using PSInzinerija1.Shared.Data.Models;
+
+
+namespace PSInzinerija1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameStatsController : ControllerBase
+    {
+        private readonly GameStatsService _gameStatsService;
+        public GameStatsController(GameStatsService gameStatsService)
+        {
+            _gameStatsService = gameStatsService;
+        }
+        [HttpGet("{game}")]
+        public async Task<ActionResult<Stats>> GetStatsAsync(AvailableGames game)
+        {
+            var stats = await gameStatsService.GetStatsAsync(HttpContext.User, game);
+
+            return stats == null ? NotFound() : Ok(stats);
+        }
+    }
+}
